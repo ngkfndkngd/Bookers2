745a9fe1588465b07ef1e9625a0c10799b531bb8
ngkfndkngd <huku.0033@gmail.com>
2021-09-19 03:22:21 +0000

    [Finish] ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  root 'homes#top'
   devise_for :users
+  resources :books
+  resources :users, only: [:show,:index,:edit,:update]
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
