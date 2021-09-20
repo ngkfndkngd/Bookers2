ebf9df44f27b53a33613fd5178a8ab3c781d2beb
ngkfndkngd <huku.0033@gmail.com>
2021-09-20 07:16:25 +0000

    [Add] sin　up log　in 編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+  def index
+    @book=Book.new
+    
+    def create
+    # １. データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to 
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  end
 end
