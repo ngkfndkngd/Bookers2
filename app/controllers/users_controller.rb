c8b2ba30f7cd8ff1818053161b31deba40f81a44
ngkfndkngd <huku.0033@gmail.com>
2021-09-21 12:29:12 +0000

    [Update] プロフィール編集　追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
   end
   
+  def edit
+    @user=User.find(params[:id])
+   
+  end
+  
   def user_params
     params.require(:user).permit(:name,:profile_image,:introduction)
   end
