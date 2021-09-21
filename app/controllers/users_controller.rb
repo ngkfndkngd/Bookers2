97a52c938a803642d6cad7b8d3ec6d73566234ab
ngkfndkngd <huku.0033@gmail.com>
2021-09-21 13:19:08 +0000

    [Add] 詳細画面　追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,12 @@ class UsersController < ApplicationController
    
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
   def user_params
     params.require(:user).permit(:name,:profile_image,:introduction)
   end
