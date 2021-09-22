916f8021d265d31513823346eb44162f5095b17c
ngkfndkngd <huku.0033@gmail.com>
2021-09-22 06:47:32 +0000

    [Add] 部分テンプレート　

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
     @users = User.all
+    @book_new = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
-    @book = Book.all
+    @book = @user.books
+    @book_new = Book.new
   end
-  
+
   def edit
     @user = User.find(params[:id])
-  
-   
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   def user_params
     params.require(:user).permit(:name,:profile_image,:introduction)
   end
