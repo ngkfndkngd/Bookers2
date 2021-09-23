c39ea802e21a33f78d81eb4351a9a489465cf086
ngkfndkngd <huku.0033@gmail.com>
2021-09-23 05:12:23 +0000

    [Update] フラッシュメッセージ実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a97a189a82d34096faba7e9a0e40d67fdea1748c3bf8002a016a32fbcd78b67e3b49f52a020512a3721c736a58a338668bd4aa256de04398697c4c3c4ee05ff8'
